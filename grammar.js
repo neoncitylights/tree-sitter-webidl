da8545c750d24b3d20b07dcc20d40bbc4cf5380d
Samantha Nguyen <contact@samanthanguyen.me>
2025-06-23 13:09:36 -0500

    feat: add terminal symbols + identifier list

--- grammar.js
+++ grammar.js
@@ -9,9 +9,28 @@
 
 module.exports = grammar({
   name: "webidl",
+  extras: $ => [
+    $.whitespace,
+    $.comment,
+  ],
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => seq(),
+
+    identifier_list: $ => seq(
+      $.identifier,
+      repeat(seq(',', $.identifier)),
+      optional(','),
+    ),
+
+    // terminal symbols
+    integer: $ => /-?([1-9][0-9]*|0[Xx][0-9A-Fa-f]+|0[0-7]*)/,
+    decimal: $ => /-?(([0-9]+\.[0-9]*|[0-9]*\.[0-9]+)([Ee][+-]?[0-9]+)?|[0-9]+[Ee][+-]?[0-9]+)/,
+    identifier: $ => /[_-]?[A-Za-z][0-9A-Z_a-z-]*/,
+    string: $ => /"[^"]*"/,
+    other: $ => /[^\t\n\r 0-9A-Za-z]/,
+    whitespace: $ => /[\t\n\r ]+/,
+    comment: $ => /\/\/.*|\/\*(.|\n)*?\*\//,
   }
 });
